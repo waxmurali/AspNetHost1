590011a85f6b5f146692f9ba797df170471f2237
waxmurali <waxmurali@gmail.com>
2018-02-21 18:57:41 +0000

    testing response object change
    testing response object change

--- AspNetHost1/Modules/DialogflowModule.cs
+++ AspNetHost1/Modules/DialogflowModule.cs
@@ -58,42 +58,42 @@ namespace TestNancy.Modules
                 //var dialogflowRequest = this.Bind<Models.DialogflowRequestV1>();
                 _logWriter.LogMessage("Handle Request");
 
-		   
-       //         var response = new DialogflowResponseV1
-			    //{
-       //             Speech = "Hi Hello World Webhook Service",
-       //             DisplayText = "Hi Hello World Webhook Service",
-       //             Source = "Webhook Service",
-       //             Data = new Data(),
-       //             ContextOut = new Context[0]
-       //             //Messages = new[]
-       //             //{
-       //             //	new Message("Test Message 1", 0),
-       //             //	new Message("Test Message 2", 0),
-       //             //	_messageGenerator.Generate(null) // random message serving no point apart from testing our Dependency Injection registrations in the Bootstrapper
-       //             //}
-       //         };
 
-                var response = new QueryResponseJsonModel()
+                var response = new DialogflowResponseV1
                 {
-                    SessionId = dialogflowRequest.SessionId,
-                    Lang = dialogflowRequest.Lang,
-                    Result = new QueryResponseResultJsonModel()
-                    {
-                        Fulfillment = new FulfillmentJsonModel()
-                        {
-                            Speech = "Hi Hello World Webhook Service",
-                            Messages = new List<FulfillmentMessageJsonModel>()
-                            {
-                                new FulfillmentMessageJsonModel() {Speech = "Hi From Full",Text = "Hi From Text",Type = 0,Replies = new List<string>() {"testinggggggg123"},Title = "Google Home Test",Subtitle = "Chumma"}
-                            }
-                        },
-                        Speech = "Hi Hello World Webhook Service",
-                        Source = "Webhook Service",
-                    }
+                    Speech = "Hi Hello World Webhook Service",
+                    DisplayText = "Hi Hello World Webhook Service",
+                    Source = "Webhook Service",
+                    Data = new Data(),
+                    ContextOut = new Context[0]
+                    //Messages = new[]
+                    //{
+                    //	new Message("Test Message 1", 0),
+                    //	new Message("Test Message 2", 0),
+                    //	_messageGenerator.Generate(null) // random message serving no point apart from testing our Dependency Injection registrations in the Bootstrapper
+                    //}
                 };
 
-                
+                //var response = new QueryResponseJsonModel()
+                //{
+                //    SessionId = dialogflowRequest.SessionId,
+                //    Lang = dialogflowRequest.Lang,
+                //    Result = new QueryResponseResultJsonModel()
+                //    {
+                //        Fulfillment = new FulfillmentJsonModel()
+                //        {
+                //            Speech = "Hi Hello World Webhook Service",
+                //            Messages = new List<FulfillmentMessageJsonModel>()
+                //            {
+                //                new FulfillmentMessageJsonModel() {Speech = "Hi From Full",Text = "Hi From Text",Type = 0,Replies = new List<string>() {"testinggggggg123"},Title = "Google Home Test",Subtitle = "Chumma"}
+                //            }
+                //        },
+                //        Speech = "Hi Hello World Webhook Service",
+                //        Source = "Webhook Service",
+                //    }
+                //};
+
+
                 return Response.AsJson(response,HttpStatusCode.OK);
             }
             catch (Exception ex)
