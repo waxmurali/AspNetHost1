1d599c74dd512afb15db07566685fccea966cd9d
waxmurali <waxmurali@gmail.com>
2018-02-22 21:45:47 +0000

    Removed existing code
    Removed existing code

--- AspNetHost1/Processing/Implementation/ProcessRequest.cs
+++ AspNetHost1/Processing/Implementation/ProcessRequest.cs
@@ -28,30 +28,6 @@ namespace AspNetHost1.Processing.Implementation
             var responseText =
                 _processRules.First(r => r.IsRequestTextMatch(request.Result.Parameters.EntityText))
                     .ProcessRequest(request, logWriter);
-            //foreach (var processRule in _processRules)
-            //{
-            //    if (processRule.IsRequestTextMatch(request.Result.Parameters.EntityText))
-            //        responseText = processRule.ProcessRequest(request, logWriter);
-            //}
-            ////var restext = _processRules.ForEach(c => c.IsRequestTextMatch(request.Result.Parameters.EntityText)).
-
-            
-            //switch (request.Result.Parameters.EntityText.ToLower())
-            //{
-            //    case "web3":
-            //        responseText = getWelcomeText(request);
-            //        break;
-            //    case "users":
-            //        responseText = getUsersText(request);
-            //        break;
-            //    case "clients":
-            //        responseText = getClientsText(request);
-            //        break;
-            //    default:
-            //        responseText = "Sorry, I could not process your request. Please try again.";
-            //        break;
-            //}
-
 
             var response = new DialogflowResponseV1
             {
@@ -63,34 +39,6 @@ namespace AspNetHost1.Processing.Implementation
             };
 
             return response;
-        }
-
-        private string getClientsText(QueryRequestJsonModel request)
-        {
-            return "200 clients are using web3 at the moment";
-        }
-
-        private string getUsersText(QueryRequestJsonModel request)
-        {
-            string responseText;
-            switch (request.Result.Parameters.PeriodRange.ToLower().Trim())
-            {
-                case "last week":
-                    responseText = "There were 20 users logged in last week";
-                    break;
-                case "month":
-                    responseText = "There were 50 users logged into web3 this month";
-                    break;
-                default:
-                    responseText = "Sorry, I could only process for last week and this month";
-                    break;
-            }
-            return responseText;
-        }
-
-        private string getWelcomeText(QueryRequestJsonModel request)
-        {
-            return $"Hi welcome to {request.Result.Parameters.EntityText} world";
         }
     }
 }
